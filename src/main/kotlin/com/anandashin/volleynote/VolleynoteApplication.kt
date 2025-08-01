6006e4ec9d923be84c0978dabf62ec94fc45fa78
anandashin <shinjigo221@snu.ac.kr>
2025-08-01 23:58:48 +0900

    fix: add open in VolleynoteApplication

--- src/main/kotlin/com/anandashin/volleynote/VolleynoteApplication.kt
+++ src/main/kotlin/com/anandashin/volleynote/VolleynoteApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class VolleynoteApplication
+open class VolleynoteApplication
 
 fun main(args: Array<String>) {
     runApplication<VolleynoteApplication>(*args)
