4bcb3061a73141dc7ae72a2c229afe17adaa77a7
anandashin <shinjigo221@snu.ac.kr>
2025-08-03 22:26:39 +0900

    add comment in UserRepository

--- src/main/kotlin/com/anandashin/volleynote/user/repository/UserRepository.kt
+++ src/main/kotlin/com/anandashin/volleynote/user/repository/UserRepository.kt
@@ -8,3 +8,4 @@ interface UserRepository: JpaRepository<UserEntity, Long> {
     fun findByNickname(nickname: String): UserEntity?
     fun existsByEmail(email: String): Boolean
 }
+// findById, existsById는 기본 제공. 따로 선언 X
